2c01a8a0676f3c5a67f24bc20563b638d7200f39
tsangsiu <tsangsiu@gmail.com>
2022-06-08 20:46:31 +0800

    OO RPS Game: Implement the Player#choose method

--- Lesson_2/OO_Rock_Paper_Scissors/OO_RPS.rb
+++ Lesson_2/OO_Rock_Paper_Scissors/OO_RPS.rb
@@ -30,12 +30,30 @@ Rule
 =end
 
 class Player
-  def initialize
-    # maybe a "name"? what about a "move"?
+  attr_accessor :move
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
   end
   
   def choose
-    
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, or scissors:"
+        choice = gets.chomp
+        break if ["rock", "paper", "scissors"].include?(choice)
+        puts "Sorry, invalid choice."
+      end
+      self.move = choice
+    else
+      self.move = ["rock", "paper", "scissors"].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
   end
 end
 
@@ -54,23 +72,34 @@ end
 
 # not sure where "compare" goes yet
 def compare(move1, move2)
-  
+
 end
 
 ### Orchestration Engine
 
 class RPSGame
   attr_accessor :human, :computer
+
   def initialize
-    @human = player.new
-    @computer = player.new
+    @human = Player.new
+    @computer = Player.new(:computer)
   end
-  
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+
   def play
     display_welcome_message
     human.choose
     computer.choose
-    display_winner
+    # display_winner
     display_goodbye_message
   end
 end
+
+RPSGame.new.play
