e6860638b4817865879a5efbf3b0c83c6114af82
tsangsiu <tsangsiu@gmail.com>
2022-06-10 04:30:57 +0000

    Done Hard 1, Lesson 4

---
+++ Lesson_4/05_Hard_1/02.rb
@@ -0,0 +1,85 @@
+module Movable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+  
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Movable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+=begin
+My original solution
+Technically it does the job, but object-wise, motorboats are not a subclass of catamaran
+=end
+
+# class Catamaran
+#   include Movable
+  
+#   attr_reader :propeller_count, :hull_count
+
+#   def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+#     self.fuel_efficiency = km_traveled_per_liter
+#     self.fuel_capacity = liters_of_fuel_capacity
+#     # ... code omitted ...
+#   end
+# end
+
+# class Motorboat < Catamaran
+#   def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+#     super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+#   end
+# end
+
+class Seacraft
+  include Movable
+  
+  attr_reader :propeller_count, :hull_count
+  
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+end
+
+class Catamaran < Seacraft
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
