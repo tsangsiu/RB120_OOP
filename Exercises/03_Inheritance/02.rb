131fa35ed0456c1407c55dcde9b9412a9f944fb8
tsangsiu <tsangsiu@gmail.com>
2022-06-15 08:54:15 +0000

    Re-do Exercises: Inheritance

--- Exercises/03_Inheritance/02.rb
+++ Exercises/03_Inheritance/02.rb
@@ -17,5 +17,26 @@ class Truck < Vehicle
   end
 end
 
+=begin
+
+# Alternate Solution
+
+class Vehicle
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+    start_engine
+  end
+end
+
+class Truck < Vehicle
+  def start_engine
+    puts 'Ready to go!'
+  end
+end
+
+=end
+
 truck1 = Truck.new(1994)
 puts truck1.year
