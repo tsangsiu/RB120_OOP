ea8304f0f5d99119363fa8d77c84d47ddf9d74cd
tsangsiu <tsangsiu@gmail.com>
2022-06-15 04:23:17 +0000

    Re-do Exercises: Classes and Objects I & II

--- Exercises/02_Classes_and_Objects_II/01.rb
+++ Exercises/02_Classes_and_Objects_II/01.rb
@@ -8,8 +8,10 @@ Cat.generic_greeting
 
 =begin
 
-If we run `kitty.class.generic_greeting`, we get the same output to the console,
-because `kitty.class` returns the class `Cat`.
+It will output "Hello! I'm a cat!" to the console.
+
+`kitty.class` returns the class of the object that `kitty` references, which is 
+`Cat`. The class method is then called on `Cat`, returning "Hello! I'm a cat!".
 
 =end
 
