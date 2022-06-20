3053e3b27f0d150917202a5fa1269dd87968d34f
tsangsiu <tsangsiu@gmail.com>
2022-06-20 07:28:10 +0000

    [RPS Game] Add the keep score functionality

--- Lesson_2/Rock_Paper_Scissors_OO/rock_paper_scissors_oo.rb
+++ Lesson_2/Rock_Paper_Scissors_OO/rock_paper_scissors_oo.rb
@@ -1,8 +1,11 @@
 class Player
-  attr_accessor :move, :name
+  attr_accessor :name, :move, :winner, :score, :grand_winner
 
   def initialize
     set_name
+    @winner = false
+    @score = 0
+    @grand_winner = false
   end
 end
 
@@ -79,6 +82,8 @@ end
 class RPSGame
   attr_accessor :human, :computer
 
+  SCORE_TO_WIN = 10
+
   def initialize
     @human = Human.new
     @computer = Computer.new
@@ -97,14 +102,55 @@ class RPSGame
     puts "#{computer.name} chose #{computer.move}."
   end
 
-  def display_winner
+  def determine_round_winner
     if human.move > computer.move
-      puts "#{human.name} won!"
+      human.winner = true
     elsif human.move < computer.move
+      computer.winner = true
+    end
+  end
+
+  def add_score
+    if human.winner
+      human.score += 1
+    elsif computer.winner
+      computer.score += 1
+    end
+  end
+
+  def display_round_winner
+    if human.winner
+      puts "#{human.name} won!"
+    elsif computer.winner
       puts "#{computer.name} won!"
     else
       puts "It's a tie!"
     end
+  end
+
+  def display_score
+    puts "#{human.name}'s score: #{human.score}"
+    puts "#{computer.name}'s score: #{computer.score}"
+  end
+
+  def determine_grand_winner
+    if human.score >= SCORE_TO_WIN
+      human.grand_winner = true
+    elsif computer.score >= SCORE_TO_WIN
+      computer.grand_winner = true
+    end
+  end
+
+  def display_grand_winner
+    if human.grand_winner
+      puts "#{human.name} is the grand winner!"
+    elsif computer.grand_winner
+      puts "#{computer.name} is the grand winner!"
+    end
+  end
+
+  def grand_winner?
+    human.score >= SCORE_TO_WIN || computer.score >= SCORE_TO_WIN
   end
 
   def play_again?
@@ -122,14 +168,49 @@ class RPSGame
     display_welcome_message
 
     loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
+      reset_game
+      until grand_winner?
+        reset_round
+        human.choose
+        computer.choose
+        display_moves
+        determine_round_winner
+        add_score
+        display_round_winner
+        display_score
+      end
+      determine_grand_winner
+      display_grand_winner
       break unless play_again?
     end
 
     display_goodbye_message
+  end
+
+  private
+
+  def reset_score
+    human.score = 0
+    computer.score = 0
+  end
+
+  def reset_grand_winner
+    human.grand_winner = false
+    computer.grand_winner = false
+  end
+
+  def reset_game
+    reset_score
+    reset_grand_winner
+  end
+
+  def reset_round_winner
+    human.winner = false
+    computer.winner = false
+  end
+
+  def reset_round
+    reset_round_winner
   end
 end
 
