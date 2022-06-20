af50710ac367c34787e533ae9ad56ade072403c4
tsangsiu <tsangsiu@gmail.com>
2022-06-20 07:51:12 +0000

    [RPS Game] Add lizard and spock

--- Lesson_2/Rock_Paper_Scissors_OO/rock_paper_scissors_oo.rb
+++ Lesson_2/Rock_Paper_Scissors_OO/rock_paper_scissors_oo.rb
@@ -24,7 +24,7 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
+      puts "Please choose rock, paper, scissors, spock, or lizard:"
       choice = gets.chomp
       break if Move::VALUES.include?(choice)
       puts "Sorry, invalid  choice."
@@ -44,7 +44,7 @@ class Computer < Player
 end
 
 class Move
-  VALUES = ['rock', 'paper', 'scissors']
+  VALUES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
   def initialize(value)
     @value = value
@@ -61,17 +61,29 @@ class Move
   def paper?
     @value == 'paper'
   end
+  
+  def spock?
+    @value == 'spock'
+  end
+  
+  def lizard?
+    @value == 'lizard'
+  end
 
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (scissors? && other_move.paper?) ||
-      (paper? && other_move.rock?)
+    (rock? && (other_move.scissors? || other_move.lizard?)) ||
+      (scissors? && (other_move.paper? || other_move.lizard?)) ||
+      (paper? && (other_move.rock? || other_move.spock?)) ||
+      (spock? && (other_move.rock? || other_move.scissors?)) ||
+      (lizard? && (other_move.paper? || other_move.spock?))
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (scissors? && other_move.rock?) ||
-      (paper? && other_move.scissors?)
+    (rock? && (other_move.paper? || other_move.spock?)) ||
+      (scissors? && (other_move.rock? || other_move.spock?)) ||
+      (paper? && (other_move.scissors? || other_move.lizard?)) ||
+      (spock? && (other_move.paper? || other_move.lizard?)) ||
+      (lizard? && (other_move.rock? || other_move.scissors?))
   end
 
   def to_s
