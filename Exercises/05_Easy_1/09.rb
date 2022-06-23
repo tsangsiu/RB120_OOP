87222e05ec031d6cfb7e7278c959778d875fc2cf
tsangsiu <tsangsiu@gmail.com>
2022-06-23 09:02:50 +0000

    Re-do #9-#10 Exercises: Easy 1

--- Exercises/05_Easy_1/09.rb
+++ Exercises/05_Easy_1/09.rb
@@ -19,3 +19,18 @@ end
 pudding = Cat.new('Pudding', 7, 'black and white')
 butterscotch = Cat.new('Butterscotch', 10, 'tan and white')
 puts pudding, butterscotch
+
+=begin
+
+# Further Exploration
+
+We would be able to omit the `initialize` method in that case because when Ruby
+looks for the `initialize` method in the class `Cat` and it doesn't have it, it
+will go to `Cat`'s superclass to look for that method.
+
+It would not be a good idea to modify `Pet` in this way because not all pets
+have fur. Also, when we add an extra parameter to `initialize` in the `Pet`
+class, we have to check all its subclasses, because they may depend on it due to
+inheritance.
+
+=end
--- Exercises/05_Easy_1/10.rb
+++ Exercises/05_Easy_1/10.rb
@@ -1,29 +1,29 @@
-class Vehicles
+class Vehicle
   attr_reader :make, :model
   
   def initialize(make, model)
     @make = make
     @model = model
   end
-
+  
   def to_s
     "#{make} #{model}"
   end
 end
 
-class Car < Vehicles
+class Car < Vehicle
   def wheels
     4
   end
 end
 
-class Motorcycle < Vehicles
+class Motorcycle < Vehicle
   def wheels
     2
   end
 end
 
-class Truck < Vehicles
+class Truck < Vehicle
   attr_reader :payload
 
   def initialize(make, model, payload)
@@ -33,5 +33,37 @@ class Truck < Vehicles
 
   def wheels
     6
+  end
+end
+
+=begin
+
+Further Exploration
+
+It would make sense to define a `wheels` method in `Vehicle` even though all of
+the remaining class would be overriding it. Implemented like that, all
+subclasses of `Vehicle` would have the `wheels` method. This would make
+polymorphism via inheritance easier.
+
+It is true that not all vehicles have wheels (boats have no wheels). Put it in
+the other way, vehicles which have no wheels have 0 wheels. Therefore, the
+`wheels` method in the `Vehicle` class would be defined like this:
+
+=end
+
+class Vehicle
+  attr_reader :make, :model
+  
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
+  
+  def wheels
+    0
+  end
+  
+  def to_s
+    "#{make} #{model}"
   end
 end
