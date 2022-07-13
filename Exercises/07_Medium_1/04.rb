80b287dd2ff0277acf1e3469e097df053acea737
tsangsiu <tsangsiu@gmail.com>
2022-07-13 07:45:41 +0000

    Done #4, Exercises: Medium 1

--- Exercises/07_Medium_1/04.rb
+++ Exercises/07_Medium_1/04.rb
@@ -1,25 +1,63 @@
-# My original solution,
-# which happens to be the solution for Further Exploration
+# My original solution, which happens to be the solution for Further Exploration
+
+# class CircularQueue
+#   def initialize(size)
+#     @circular_queue = []
+#     @size = size
+#   end
+
+#   def dequeue
+#     return nil if @circular_queue.empty?
+#     @circular_queue.shift
+#   end
+
+#   def enqueue(element)
+#     dequeue if @circular_queue.size >= @size
+#     @circular_queue << element
+#   end
+# end
 
 class CircularQueue
   def initialize(size)
-    @circular_queue = []
+    @queue = Array.new(size)
     @size = size
+    @start = 0
+    @end = 0
   end
 
   def dequeue
-    return nil if @circular_queue.empty?
-    @circular_queue.shift
+    return nil if empty?
+    element_dequeued = @queue[@start]
+    @queue[@start] = nil
+    @start = next_index(@start)
+    element_dequeued
   end
 
   def enqueue(element)
-    dequeue if @circular_queue.size >= @size
-    @circular_queue << element
+    if empty?
+      @queue[@start] = element
+    else
+      @end = next_index(@end)
+      @queue[@end] = element
+      if @end == @start && @queue[next_index(@start)] != nil
+        @start = next_index(@start)
+      end
+    end
   end
 
-  # def to_s
-  #   @circular_queue.to_s
-  # end
+  def empty?
+    @queue.all?(&:nil?)
+  end
+
+  def to_s
+    @queue.to_s
+  end
+
+  private
+
+  def next_index(index)
+    (index + 1) % @size
+  end
 end
 
 queue = CircularQueue.new(3)
