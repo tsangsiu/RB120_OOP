6abd8df988b4112794a630395fb5542639cbb2ba
tsangsiu <tsangsiu@gmail.com>
2022-07-14 22:17:06 +0800

    Done #1-#2, Exercises: Debugging

---
+++ Exercises/08_Debugging/01.rb
@@ -0,0 +1,55 @@
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def display_data
+    puts "---------------"
+    puts "Title: #{title}"
+    puts "Author: #{author}"
+    puts "ISBN: #{isbn}"
+    puts "---------------"
+  end
+end
+
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+
+# community_library.books.display_data
+
+=begin
+
+On line 42, it is intended to display information regarding the books currently
+checked in to the current library. `community_library.books` returns an array
+of `Book` object. However, the method `display_data` is a book's behaviour, not
+array's. In order to display the information of each book, we should do:
+  
+=end
+
+community_library.books.each do |book|
+  book.display_data
+end
---
+++ Exercises/08_Debugging/02.rb
@@ -0,0 +1,63 @@
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+=begin
+
+# Further Exploration
+
+The `FlightlessBird#initialize` method is not necessary. When it is called, by
+the method lookup chain, the `super` keyword will eventually lead to invocation
+of `Animal#initialize` method with all arguments pass to it.
+
+Without the `FlightlessBird` method, Ruby will still invoke `Animal#initialize`
+by the method lookup chain.
+
+=end
