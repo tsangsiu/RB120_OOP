4ab3cb5c8621dd93e050e68535f6c30d91d59973
tsangsiu <tsangsiu@gmail.com>
2023-01-18 03:38:46 +0000

    Re-do Debugging Ex. #4-#5

--- Exercises/08_Debugging/05.rb
+++ Exercises/08_Debugging/05.rb
@@ -5,15 +5,14 @@ class File
     @name = name
   end
 
-  # to alias `read`/`write` to `byte_content`/`byte_content=` inside the `File` class
   alias_method :read,  :byte_content
   alias_method :write, :byte_content=
 
   def copy(target_file_name)
     target_file = self.class.new(target_file_name)
-    target_file.write(read) # the method `read` returns the byte contents 
+    target_file.write(read)
 
-    target_file # returns a MarkdownFile object
+    target_file
   end
 
   def to_s
@@ -36,12 +35,24 @@ end
 # Test
 
 blog_post = MarkdownFile.new('Adventures_in_OOP_Land')
-blog_post.write('Content will be added soon!'.bytes) # calling the setter of the instance variable @byte_content
-# the `bytes` method returns the array of ASCII code of characters
+blog_post.write('Content will be added soon!'.bytes)
 
 copy_of_blog_post = blog_post.copy('Same_Adventures_in_OOP_Land')
 
 puts copy_of_blog_post.is_a? MarkdownFile     # true
 puts copy_of_blog_post.read == blog_post.read # true
 
-puts blog_post # => (expected) Adventures_in_OOP_Land.md
+puts blog_post
+
+=begin
+On line 45, when the `puts` method is invoked with the argument `blog_post`, the
+`to_s` method is implicitly called on `blog_post`, returning `#{name}.#{FORMAT}`.
+To resolve the constant `FORMAT`, Ruby first looks for it in its surrounding
+structure. The constant is not found in the `to_s` method or the `File` class, 
+Ruby then looks for it up in the method lookup path, and finally in the main
+scope. As the constant is still not found, Ruby raises `NameError`.
+
+To resolve the problem, we can specifically specify where to look for the
+constant, like so:
+`"#{name}.#{self.class::FORMAT}"`
+=end
